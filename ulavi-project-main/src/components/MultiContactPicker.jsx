5ee1468e18e032e92cd88ee5c7caa30145a444a1
Dhanushyan J <138801703+Dhanushyan2508@users.noreply.github.com>
2026-06-16 23:40:42 +0530

    Delete ulavi-project-main directory

--- ulavi-project-main/src/components/MultiContactPicker.jsx
+++
@@ -1,3 +0,0 @@
-export { default as ContactSelectionModal } from './ContactSelectionModal';
-export { default as PhoneOptionCard } from './PhoneOptionCard';
-export { default as EmailOptionCard } from './EmailOptionCard';
